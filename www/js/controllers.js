967d805a0f7dfe99a58d8346979b77c0216db6fb
lamrous <lamrous.lyes@gmail.com>
2015-11-24 10:09:40 -0500

    Add access to the REST services

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['starter.services'])
 
 .controller('DashCtrl', function($scope, Chokes) {
     $scope.chokes = Chokes.all();
--- www/js/services.js
+++ www/js/services.js
@@ -1,35 +1,8 @@
-angular.module('starter.services', [])
+angular.module('starter.services', ['ngResource'])
 
-.factory('Chokes', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Chokes', function($resource) {
 
-  // Some fake testing data
-  var chokes = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+  var chokes =  $resource('http://localhost:3000/api/chokes');
 
   return {
     all: function() {
@@ -46,37 +19,10 @@ angular.module('starter.services', [])
   };
 })
 
-.factory('Events', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Events', function($ressource) {
 
-  // Some fake testing data
-  var events = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
+  var events = $resource('http://localhost:3000/api/myEvents');
+  console.log(events);
   return {
     all: function() {
       return events;
